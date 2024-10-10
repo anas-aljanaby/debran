42ef5faf7bd486a5d916a1ed2ef3b84c4449b992
anas-aljanaby <anas.aljanaby00@gmail.com>
2024-10-11 00:11:02 +0300

    uncomment cover image code

--- components/editor.tsx
+++ components/editor.tsx
@@ -4,7 +4,7 @@ import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
 import { useTheme } from "next-themes";
-// import { useEdgeStore } from "@/lib/edgestore";
+import { useEdgeStore } from "@/lib/edgestore";
 import "@blocknote/core/style.css";
 import "@blocknote/mantine/style.css";
 
@@ -17,21 +17,23 @@ interface EditorProps {
 const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
 
-//   const { edgestore } = useEdgeStore();
+  const { edgestore } = useEdgeStore();
 
-//   const handleUpload = async (file: File) => {
-//     const res = await edgestore.publicFiles.upload({
-//       file,
-//     });
+  const handleUpload = async (file: File) => {
+    const res = await edgestore.publicFiles.upload({
+      file,
+    });
 
-//     return res.url;
-//   };
+    return res.url;
+  };
+
+  console.log(initialContent);
 
   const editor: BlockNoteEditor = useCreateBlockNote({
     initialContent: initialContent
       ? (JSON.parse(initialContent) as PartialBlock[])
       : undefined,
-    // uploadFile: handleUpload,
+    uploadFile: handleUpload,
   });
 
   const handleEditorChange = () => {
